312d6d717634503c52a8fc568842f2b142aed345
hamidreza Ranjbarpour <hamidrezarrj@gmail.com>
2021-07-11 16:46:00 +0430

    back button added & bug on active tabs fixed

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,17 +1,26 @@
 <!-- Navbar -->
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="{{url()->previous()}}"><i class="fas fa-arrow-left"></i></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      @if(url()->current() == route('home'))
+        <li class="nav-item active">
+      @else
+        <li class="nav-item">
+      @endif
         <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+
+      @if(url()->current() == route('create'))
+        <li class="nav-item active">
+      @else
+        <li class="nav-item">
+      @endif
         <a class="nav-link" href="{{route('create')}}">Add Students</a>
       </li>
 
